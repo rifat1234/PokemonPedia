0314e9a81f2cfb82df463e3a9a5bc7416afe10f6
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-28 23:21:59 +0100

    PokemonCellView added

--- Pokemon/Model/Pokemon.swift
+++ Pokemon/Model/Pokemon.swift
@@ -10,4 +10,14 @@ import Foundation
 struct Pokemon: Codable, Equatable {
     let name:String
     let url:String
+    let imageURL:String
+    
+    init(name: String, url: String) {
+        self.name = name
+        self.url = url
+        
+        let strings =  url.split(separator: "/")
+        let num = strings[strings.count - 1]
+        self.imageURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(num).png"
+    }
 }
---
+++ Pokemon/View/PokemonCellView.swift
@@ -0,0 +1,42 @@
+//
+//  PokemonView.swift
+//  Pokemon
+//
+//  Created by Rifat Monzur on 28/6/24.
+//
+
+import SwiftUI
+import NukeUI
+
+struct PokemonCellView: View {
+    struct Const {
+        static let pokemonImageSize:CGFloat = 30
+    }
+    
+    let pokemon: Pokemon
+    var body: some View {
+        HStack{
+            LazyImage(
+                url: URL(string: pokemon.imageURL)
+            ){
+                state in
+                    if let image = state.image {
+                        image.resizable().aspectRatio(contentMode: .fit)
+                    } else if state.error != nil {
+                        Image(systemName: "questionmark.circle")
+                            .tint(.red)
+                    } else {
+                        ProgressView()
+                    }
+            }
+            .frame(width:Const.pokemonImageSize, height:Const.pokemonImageSize)
+            
+            Text(pokemon.name)
+    
+        }
+    }
+}
+
+#Preview {
+    PokemonCellView(pokemon: Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/"))
+}
