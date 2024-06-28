7b8ee2b265fd09fb18e37940348d4cc57e02efd5
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-28 22:34:03 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Rifat Monzur on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
