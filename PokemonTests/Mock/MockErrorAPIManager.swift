4d73486c99662bb299130cc69b6b0913c9a26945
Rifat Monzur <rifatmonzur@gmail.com>
2024-06-30 21:54:05 +0100

    PokemonListVM fetchAllPokemon error test added

---
+++ PokemonTests/Mock/MockErrorAPIManager.swift
@@ -0,0 +1,22 @@
+//
+//  MockErrorAPIManager.swift
+//  PokemonTests
+//
+//  Created by Rifat Monzur on 30/6/24.
+//
+
+import Foundation
+
+struct MockErrorAPIManager: APIManagerProtocol {
+    private enum MockError: Error{
+        case mock
+    }
+    
+    func fetchPokemonDetails(url: String) async throws -> PokemonDetails {
+        throw MockError.mock
+    }
+
+    func fetchAllPokemon() async throws -> [Pokemon] {
+        throw MockError.mock
+    }
+}
--- PokemonTests/PokemonListVMTests.swift
+++ PokemonTests/PokemonListVMTests.swift
@@ -12,9 +12,9 @@ final class PokemonListVMTests: XCTestCase {
 
     override func setUpWithError() throws {
         let apiManager =  MockAPIManager(allPokemon: [
-            Pokemon(name: "aab", url: "url1"),
             Pokemon(name: "abc", url: "url2"),
-            Pokemon(name: "pika", url: "url3"),
+            Pokemon(name: "aab", url: "url1"),
+            Pokemon(name: "pika", url: "url3")
         ])
         
         viewModel = PokemonListView.ViewModel(apiManger: apiManager)
@@ -22,6 +22,24 @@ final class PokemonListVMTests: XCTestCase {
 
     override func tearDownWithError() throws {
         
+    }
+    
+    func testFetchAllPokemonWithAlphabeticalOrder() async throws {
+        await viewModel.fetchAllPokemons()
+        
+        let testResult = [
+            Pokemon(name: "aab", url: "url1"),
+            Pokemon(name: "abc", url: "url2"),
+            Pokemon(name: "pika", url: "url3"),
+        ]
+        
+        XCTAssertEqual(testResult, viewModel.searchedPokemons)
+    }
+    
+    func testFetchAllPokemonError() async throws {
+        viewModel = PokemonListView.ViewModel(apiManger: MockErrorAPIManager())
+        await viewModel.fetchAllPokemons()
+        XCTAssertTrue(viewModel.showAlert)
     }
 
     func testSearchResult() async throws {
@@ -42,23 +60,11 @@ final class PokemonListVMTests: XCTestCase {
         
         XCTAssertEqual(testResult2, viewModel.searchedPokemons)
     }
-    
-    func testSearchResultAlphabeticalOrder() async throws {
-        await viewModel.fetchAllPokemons()
-        
-        let testResult = [
-            Pokemon(name: "aab", url: "url1"),
-            Pokemon(name: "abc", url: "url2"),
-            Pokemon(name: "pika", url: "url3"),
-        ]
-        
-        XCTAssertEqual(testResult, viewModel.searchedPokemons)
-    }
-    
+
     func testViewStateChange() async throws {
         XCTAssertEqual(viewModel.viewState, .dataLoading)
         await viewModel.fetchAllPokemons()
         XCTAssertEqual(viewModel.viewState, .dataLoaded)
     }
-
+        
 }
