5827cc9f4a49f8ebcc7c2aedb1b2da42a3240751
Rifat Monzur <rifatmonzur@gmail.com>
2024-07-01 19:09:12 +0100

    PokemonDetail StatSection Icon added

--- Pokemon/View/PokemonDetailView/SubViews/StatSection.swift
+++ Pokemon/View/PokemonDetailView/SubViews/StatSection.swift
@@ -8,6 +8,35 @@
 import SwiftUI
 
 extension PokemonDetailsView {
+    enum Icon: String {
+        case hp
+        case attack
+        case defense
+        case specialAttack = "special attack"
+        case specialDefense = "special defense"
+        case speed
+        case unknown
+        
+        var imageName:String {
+            switch self {
+            case .hp:
+                "figure.tennis"
+            case .attack:
+                "figure.boxing"
+            case .defense:
+                "hand.raised"
+            case .specialAttack:
+                "figure.barre"
+            case .specialDefense:
+                "hand.raised.fill"
+            case .speed:
+                "figure.run"
+            case .unknown:
+                "figure.run"
+            }
+        }
+    }
+    
     struct StatsSection: View {
         let stats: [Stat]?
         var body: some View {
@@ -15,8 +44,11 @@ extension PokemonDetailsView {
                 Section("Stats") {
                     ForEach(stats, id: \.self) { stat in
                         HStack {
-                            Text(stat.stat?.name ?? "")
-                                .font(.headline)
+                            HStack {
+                                Image(systemName: (Icon(rawValue: stat.stat?.name?.lowercased() ?? "unknown") ?? .unknown).imageName)
+                                    .frame(width:20)
+                                Text(stat.stat?.name ?? "")
+                            }
                             Spacer()
                             VStack {
                                 Group {
@@ -25,7 +57,7 @@ extension PokemonDetailsView {
                                 }
                                 .frame(maxWidth: .infinity, alignment: .trailing)
                             }
-                            .font(.caption)
+                            .font(.caption2)
                             
                         }
                     }
