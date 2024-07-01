3c0a16c9691f2f022ad9a2ce2ae29e9dcb574d7b
Rifat Monzur <rifatmonzur@gmail.com>
2024-07-01 18:36:06 +0100

    PokemonDetails BasicSection icon added

--- Pokemon/View/PokemonDetailView/SubViews/BasicSection.swift
+++ Pokemon/View/PokemonDetailView/SubViews/BasicSection.swift
@@ -16,28 +16,39 @@ extension PokemonDetailsView {
         
         var body: some View {
             Section("Basic") {
-                ListView(label: "Base Experience", value: baseExperience)
-                ListView(label: "Height", value: height, unit: " dm")
-                ListView(label: "Weight", value: weight, unit: " hg")
+                ListView(label: "Height", value: height, unit: " dm", icon: .figure)
+                ListView(label: "Weight", value: weight, unit: " hg", icon: .weight)
+                ListView(label: "Base Experience", value: baseExperience, icon: .experience)
             }
         }
     }
     
     private struct ListView: View {
+        enum Icon: String {
+            case figure
+            case weight = "scalemass"
+            case experience = "hourglass"
+        }
         let label: String
         let value: String?
         let unit: String
+        let icon: Icon
 
-        init(label: String, value: Int?, unit: String = "") {
+        init(label: String, value: Int?, unit: String = "", icon: Icon = .figure) {
             self.label = label
             self.value = (value != nil) ? String(value ?? 0): nil
             self.unit = unit
+            self.icon = icon
         }
         
         var body: some View {
             if let value = value {
                 HStack {
-                    Text(label)
+                    HStack {
+                        Image(systemName: icon.rawValue)
+                            .frame(width: 20)
+                        Text(label)
+                    }
                     Spacer()
                     Text("\(value)\(unit)")
                 }
