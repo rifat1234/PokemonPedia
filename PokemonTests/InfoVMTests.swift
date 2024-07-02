40a11f1826a1c9e118da2471f3c5bdb1ed153a8e
Rifat Monzur <rifatmonzur@gmail.com>
2024-07-02 20:22:43 +0100

    In unit test InfoView renamed to  InfoViewData

--- PokemonTests/InfoVMTests.swift
+++ PokemonTests/InfoVMTests.swift
@@ -8,11 +8,11 @@
 import XCTest
 
 final class InfoVMTests: XCTestCase {
-    var infoVM:InfoView.ViewModel?
+    var infoVM:InfoDataView.ViewModel?
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        infoVM = InfoView.ViewModel(infoData: InfoData(type: .abilities,
+        infoVM = InfoDataView.ViewModel(infoData: InfoData(type: .abilities,
                                     infos:[Info(name: "B", url: "url2"),
                                            Info(name: "A", url: "url1"),
                                            Info(name: "C", url: "url3")]))
